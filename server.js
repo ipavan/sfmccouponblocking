b893d0e0425e312fcd27dc38431da4a1bbcd9525
Pavan Sadarangani <psadarangani@psadaranga-ltm4.internal.salesforce.com>
2019-04-09 22:02:29 +0100

    updates

--- server.js
+++ server.js
@@ -73,3 +73,15 @@ app.get('/getDataExtensions', (req, res) => {
 
     res.send('200');
 });
+
+
+app.get('/transformXML', (req, res) => {
+
+	let xml = `<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><env:Header xmlns:env="http://www.w3.org/2003/05/soap-envelope"><wsa:Action>RetrieveResponse</wsa:Action><wsa:MessageID>urn:uuid:54e02905-d8de-438d-abc6-2101e9b6f898</wsa:MessageID><wsa:RelatesTo>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</wsa:RelatesTo><wsa:To>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</wsa:To><wsse:Security><wsu:Timestamp wsu:Id="Timestamp-d31f8963-4836-446a-babb-c5d74eb61b70"><wsu:Created>2019-04-09T20:58:34Z</wsu:Created><wsu:Expires>2019-04-09T21:03:34Z</wsu:Expires></wsu:Timestamp></wsse:Security></env:Header><soap:Body><RetrieveResponseMsg xmlns="http://exacttarget.com/wsdl/partnerAPI"><OverallStatus>OK</OverallStatus><RequestID>ddf26568-1f17-4748-9f29-dc15c1cd273c</RequestID><Results xsi:type="DataFolder"><PartnerKey xsi:nil="true" /><ID>830150</ID><ObjectID xsi:nil="true" /><Name>Data Extensions</Name><ContentType>dataextension</ContentType></Results><Results xsi:type="DataFolder"><PartnerKey xsi:nil="true" /><ID>916439</ID><ObjectID xsi:nil="true" /><ParentFolder><PartnerKey xsi:nil="true" /><ObjectID xsi:nil="true" /><AllowChildren>true</AllowChildren></ParentFolder><Name>Data Model</Name><ContentType>dataextension</ContentType></Results><Results xsi:type="DataFolder"><PartnerKey xsi:nil="true" /><ID>916440</ID><ObjectID xsi:nil="true" /><ParentFolder><PartnerKey xsi:nil="true" /><ObjectID xsi:nil="true" /><AllowChildren>true</AllowChildren></ParentFolder><Name>System</Name><ContentType>dataextension</ContentType></Results><Results xsi:type="DataFolder"><PartnerKey xsi:nil="true" /><ID>916441</ID><ObjectID xsi:nil="true" /><ParentFolder><PartnerKey xsi:nil="true" /><ObjectID xsi:nil="true" /><AllowChildren>true</AllowChildren></ParentFolder><Name>Segmentation</Name><ContentType>dataextension</ContentType></Results><Results xsi:type="DataFolder"><PartnerKey xsi:nil="true" /><ID>919156</ID><ObjectID xsi:nil="true" /><ParentFolder><PartnerKey xsi:nil="true" /><ObjectID xsi:nil="true" /><AllowChildren>true</AllowChildren></ParentFolder><Name>Vouchers</Name><ContentType>dataextension</ContentType></Results><Results xsi:type="DataFolder"><PartnerKey xsi:nil="true" /><ID>919166</ID><ObjectID xsi:nil="true" /><ParentFolder><PartnerKey xsi:nil="true" /><ObjectID xsi:nil="true" /><AllowChildren>true</AllowChildren></ParentFolder><Name>VoucherConfig</Name><ContentType>dataextension</ContentType></Results></RetrieveResponseMsg></soap:Body></soap:Envelope>`;
+
+	parseString(xml, function (err, result) {
+	    console.log(result);
+	    console.log(util.inspect(result, false, null));
+	});
+
+});
