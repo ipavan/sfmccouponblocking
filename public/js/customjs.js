60555357f18f4ab71e8dc15dc4c2e45c387c31fe
Pavan Sadarangani <psadarangani@psadaranga-ltm4.internal.salesforce.com>
2019-04-10 13:20:52 +0100

    updates

---
+++ public/js/customjs.js
@@ -0,0 +1,99 @@
+$(document).ready(function(){
+
+	var sdk = new window.sfdc.BlockSDK();
+
+	updatePicklistOptions();
+
+	function updatePicklistOptions() {
+		$('#options').hide();
+		$('#updateButton').hide();
+		$('#radiobuttons').hide();
+        $.ajax({
+         type: "GET",
+         url: "/getDataExtensions",
+         success: function(res) {
+            console.log('Success');
+            console.log(res);
+            $('#spinna').hide();
+            $('#options').show();
+            $('#updateButton').show();
+            $('#radiobuttons').show();
+
+             
+            let output = `
+							 	<div id="replaceable">
+								  <label class="slds-form-element__label" for="select-01">Select Label</label>
+								  <div class="slds-form-element__control">
+								    <div class="slds-select_container">
+								      <select class="slds-select" id="dename">
+								        <option value="Unselected">Select a Data Extension</option>`;
+
+	        res.forEach(function (item) {
+					    output+=`<option value="${item}">${item}</option>`
+					});
+
+	        output += `</select>
+						    </div>
+						  </div>
+						</div>`;
+
+
+			$('#replaceable').replaceWith(output);
+            
+        	}
+        });
+    }
+
+    $("#updateCoupon").click(function(){
+        setSDKData();
+    });
+
+    function setSDKData() {
+    	var sel = $('#dename').val();
+    	console.log(sel);
+    	console.log($("#radio-11").is(":checked"))
+    	console.log($("#radio-12").is(":checked"))
+
+    	let newCoupon = $("#radio-11").is(":checked");
+
+    	if (newCoupon) {
+    		let output = `<table align="center" border="0" cellpadding="0" cellspacing="0" class="tmp--container" style="background-color:#ffffff;" width="100%">
+						    <tr>
+						      <td align="left" style="color:#4f4f4f;font-family:Arial, sans-serif;font-size:16px;font-style:normal;font-weight:normal;line-height:20px;padding:20px;text-align:center;vertical-align:top;" width="100%">
+						        <span style="color:#DD0000;"><span style="display: inline-block; font-family: Arial, sans-serif; font-size: 18px; font-style: normal; font-weight: ; padding: 5px 0px 10px;">Ihr Voucher-Code: </span></span> 
+						        <!-- ============================== -->
+						        <!-- CTA BTN -->
+						        <!-- ============================== -->
+						        <table align="center" class="tmp--full-width" width="100%">
+						          <tr>
+						            <td align="center" style="padding:5px 0 10px 0;">
+						              <span style="font-family:courier new,courier,monospace;"><a href="" style="border-radius:3px;background-color:#DD0000;border-top:12px solid #DD0000;border-bottom:12px solid #DD0000;border-right:18px solid #DD0000;border-left:18px solid #DD0000;color:#ffffff;display:inline-block;font-family:Arial, sans-serif;font-size:16px;font-style:normal;font-weight:normal;line-height:16px;text-align:center;text-decoration:none;" target="_blank"><b>%%[ SET @CustomerCoupon = LookupOrderedRows('${sel}', 1, 'CouponCode DESC', 'EmailAddress', emailaddr) VAR @Coupon VAR @IsUsed IF RowCount(@CustomerCoupon) == 0 THEN SET @CouponRow = ClaimRow('${sel}', 'IsClaimed', 'EmailAddress', emailaddr, 'Validity', DateAdd(NOW(), '31', 'D')) IF EMPTY(@CouponRow) THEN SET @Coupon = 'No coupons available' ELSE SET @Coupon = FIELD(@CouponRow, 'CouponCode') ENDIF ELSE SET @Coupon = Lookup('${sel}', 'CouponCode', 'EmailAddress', emailaddr) SET @IsUsed = Lookup('${sel}', 'UsedDate', 'CouponCode', @Coupon) IF EMPTY(@IsUsed) THEN SET @ValidDate = Lookup('Gutschein_OTTO_April_2019', 'Validity', 'EmailAddress', emailaddr) IF DateDiff(NOW(), @ValidDate, 'MI') < 0 THEN SET @Coupon = 'Coupon is expired.' ENDIF ELSE SET @Coupon = 'Sie haben Ihren Coupon schon genutzt' ENDIF ENDIF ]%% %%=v(@Coupon)=%%</b></a></span>
+						            </td>
+						          </tr>
+						        </table>
+						      </td>
+						    </tr>
+						  </table>`
+    		sdk.setContent(output);
+    	} else {
+    		let output = `<table align="center" border="0" cellpadding="0" cellspacing="0" class="tmp--container" style="background-color:#ffffff;" width="100%">
+						    <tr>
+						      <td align="left" style="color:#4f4f4f;font-family:Arial, sans-serif;font-size:16px;font-style:normal;font-weight:normal;line-height:20px;padding:20px;text-align:center;vertical-align:top;" width="100%">
+						        <span style="color:#DD0000;"><span style="display: inline-block; font-family: Arial, sans-serif; font-size: 18px; font-style: normal; font-weight: ; padding: 5px 0px 10px;">Ihr Voucher-Code: </span></span> 
+						        <!-- ============================== -->
+						        <!-- CTA BTN -->
+						        <!-- ============================== -->
+						        <table align="center" class="tmp--full-width" width="100%">
+						          <tr>
+						            <td align="center" style="padding:5px 0 10px 0;">
+						              <span style="font-family:courier new,courier,monospace;"><a href="" style="border-radius:3px;background-color:#DD0000;border-top:12px solid #DD0000;border-bottom:12px solid #DD0000;border-right:18px solid #DD0000;border-left:18px solid #DD0000;color:#ffffff;display:inline-block;font-family:Arial, sans-serif;font-size:16px;font-style:normal;font-weight:normal;line-height:16px;text-align:center;text-decoration:none;" target="_blank"><b>%%[ SET @CouponRow = ClaimRow('${sel}', 'IsClaimed', 'EmailAddress', emailaddr, 'Validity', DateAdd(NOW(), '31', 'D')) IF EMPTY(@CouponRow) THEN SET @Coupon = 'No coupons available' ELSE SET @Coupon = FIELD(@CouponRow, 'CouponCode') ENDIF ]%% %%=v(@Coupon)=%%</b></a></span>
+						            </td>
+						          </tr>
+						        </table>
+						      </td>
+						    </tr>
+						  </table>`
+    	}
+    }
+});
+
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 // Serve your app
 console.log('Served: http://localhost:' + port);
 app.listen(port);
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 const options = {
@@ -38,11 +38,11 @@ app.get('/getDataExtensions', (req, res) => {
         }
 
     request(options)
-    	.then(function(res) {
-    		const accessToken = res['access_token'];
+    	.then(function(resp) {
+    		const accessToken = resp['access_token'];
             console.log(`Access Token is ${accessToken}`);
 
-            const xml = `<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><s:Header><a:Action s:mustUnderstand="1">Retrieve</a:Action><a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID><a:ReplyTo><a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address></a:ReplyTo><a:To s:mustUnderstand="1">https://mct181ddvnpf05z2r5mcclnpt34q.soap.marketingcloudapis.com/Service.asmx</a:To><fueloauth xmlns="http://exacttarget.com">${accessToken}</fueloauth></s:Header><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI"><RetrieveRequest><ObjectType>DataFolder</ObjectType><Properties>Name</Properties><Properties>ID</Properties><Properties>ContentType</Properties><Properties>ParentFolder.AllowChildren</Properties> <Filter xsi:type="par:SimpleFilterPart" xmlns:par="http://exacttarget.com/wsdl/partnerAPI"><Property>ContentType</Property><SimpleOperator>equals</SimpleOperator><Value>dataextension</Value></Filter></RetrieveRequest></RetrieveRequestMsg></s:Body></s:Envelope>`
+            const xml = `<?xml version="1.0" encoding="UTF-8"?><s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><s:Header><a:Action s:mustUnderstand="1">Retrieve</a:Action><a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID><a:ReplyTo><a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address></a:ReplyTo><a:To s:mustUnderstand="1">https://mct181ddvnpf05z2r5mcclnpt34q.soap.marketingcloudapis.com/Service.asmx</a:To><fueloauth xmlns="http://exacttarget.com">${accessToken}</fueloauth></s:Header><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI"><RetrieveRequest><ObjectType>DataExtension</ObjectType><Properties>Name</Properties><Filter xsi:type="SimpleFilterPart"><Property>CategoryID</Property><SimpleOperator>equals</SimpleOperator><Value>919156</Value> </Filter></RetrieveRequest></RetrieveRequestMsg></s:Body></s:Envelope>`
 
             //https://mct181ddvnpf05z2r5mcclnpt34q.soap.marketingcloudapis.com/etframework.wsdl
             var soapOptions = {
@@ -55,12 +55,18 @@ app.get('/getDataExtensions', (req, res) => {
 				    json: false // automatically stringifys body to json if true
 				};
 
+			let voucherDEs = [];
 			request(soapOptions)
 				.then(function(response) {
-					console.log(response);
 					parseString(response, function (err, result) {
-					    console.log(result);
-					    console.log(result['soap:Body']);
+					    let envelope = result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0];
+					    let results = envelope['Results'];
+					    results.forEach(function (item) {
+					    	let name = item['Name'][0]
+						    //console.log(name);
+						    voucherDEs.push(name);
+						});
+						res.send(voucherDEs);
 					});
 				})
 				.catch(function(error) {
@@ -70,21 +76,27 @@ app.get('/getDataExtensions', (req, res) => {
     	.catch(function(err){
     		console.log(err);
     	});
-
-    res.send('200');
 });
 
 
 app.get('/transformXML', (req, res) => {
 
-	let xml = `<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><env:Header xmlns:env="http://www.w3.org/2003/05/soap-envelope"><wsa:Action>RetrieveResponse</wsa:Action><wsa:MessageID>urn:uuid:54e02905-d8de-438d-abc6-2101e9b6f898</wsa:MessageID><wsa:RelatesTo>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</wsa:RelatesTo><wsa:To>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</wsa:To><wsse:Security><wsu:Timestamp wsu:Id="Timestamp-d31f8963-4836-446a-babb-c5d74eb61b70"><wsu:Created>2019-04-09T20:58:34Z</wsu:Created><wsu:Expires>2019-04-09T21:03:34Z</wsu:Expires></wsu:Timestamp></wsse:Security></env:Header><soap:Body><RetrieveResponseMsg xmlns="http://exacttarget.com/wsdl/partnerAPI"><OverallStatus>OK</OverallStatus><RequestID>ddf26568-1f17-4748-9f29-dc15c1cd273c</RequestID><Results xsi:type="DataFolder"><PartnerKey xsi:nil="true" /><ID>830150</ID><ObjectID xsi:nil="true" /><Name>Data Extensions</Name><ContentType>dataextension</ContentType></Results><Results xsi:type="DataFolder"><PartnerKey xsi:nil="true" /><ID>916439</ID><ObjectID xsi:nil="true" /><ParentFolder><PartnerKey xsi:nil="true" /><ObjectID xsi:nil="true" /><AllowChildren>true</AllowChildren></ParentFolder><Name>Data Model</Name><ContentType>dataextension</ContentType></Results><Results xsi:type="DataFolder"><PartnerKey xsi:nil="true" /><ID>916440</ID><ObjectID xsi:nil="true" /><ParentFolder><PartnerKey xsi:nil="true" /><ObjectID xsi:nil="true" /><AllowChildren>true</AllowChildren></ParentFolder><Name>System</Name><ContentType>dataextension</ContentType></Results><Results xsi:type="DataFolder"><PartnerKey xsi:nil="true" /><ID>916441</ID><ObjectID xsi:nil="true" /><ParentFolder><PartnerKey xsi:nil="true" /><ObjectID xsi:nil="true" /><AllowChildren>true</AllowChildren></ParentFolder><Name>Segmentation</Name><ContentType>dataextension</ContentType></Results><Results xsi:type="DataFolder"><PartnerKey xsi:nil="true" /><ID>919156</ID><ObjectID xsi:nil="true" /><ParentFolder><PartnerKey xsi:nil="true" /><ObjectID xsi:nil="true" /><AllowChildren>true</AllowChildren></ParentFolder><Name>Vouchers</Name><ContentType>dataextension</ContentType></Results><Results xsi:type="DataFolder"><PartnerKey xsi:nil="true" /><ID>919166</ID><ObjectID xsi:nil="true" /><ParentFolder><PartnerKey xsi:nil="true" /><ObjectID xsi:nil="true" /><AllowChildren>true</AllowChildren></ParentFolder><Name>VoucherConfig</Name><ContentType>dataextension</ContentType></Results></RetrieveResponseMsg></soap:Body></soap:Envelope>`;
+	let xml = `<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><env:Header xmlns:env="http://www.w3.org/2003/05/soap-envelope"><wsa:Action>RetrieveResponse</wsa:Action><wsa:MessageID>urn:uuid:3a7f63ad-cec5-4485-a0f5-ed3ae1bea760</wsa:MessageID><wsa:RelatesTo>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</wsa:RelatesTo><wsa:To>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</wsa:To><wsse:Security><wsu:Timestamp wsu:Id="Timestamp-fde72d80-69bc-4865-8a8b-e40adaaf5d05"><wsu:Created>2019-04-10T10:11:40Z</wsu:Created><wsu:Expires>2019-04-10T10:16:40Z</wsu:Expires></wsu:Timestamp></wsse:Security></env:Header><soap:Body><RetrieveResponseMsg xmlns="http://exacttarget.com/wsdl/partnerAPI"><OverallStatus>OK</OverallStatus><RequestID>92b4e3f3-9262-4190-8bfe-0f3455535e12</RequestID><Results xsi:type="DataExtension"><PartnerKey xsi:nil="true" /><ObjectID xsi:nil="true" /><Name>Gutschein_OTTO_April_2019</Name></Results><Results xsi:type="DataExtension"><PartnerKey xsi:nil="true" /><ObjectID xsi:nil="true" /><Name>Gutschein_ASOS_April_2019</Name></Results></RetrieveResponseMsg></soap:Body></soap:Envelope>`;
 
 	parseString(xml, function (err, result) {
-	    console.log(result);
-	    console.log(JSON.stringify(result));
-	    console.log(result['soap:Envelope']['$']['soap:Body']);
+	    let envelope = result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0];
+	    let results = envelope['Results'];
+	    console.log(results);
+	    console.log(JSON.stringify(results));
+	    let voucherDEs = [];
+	    results.forEach(function (item) {
+	    	let name = item['Name'][0]
+		    console.log(name);
+		    voucherDEs.push(name);
+		});
+		res.send(voucherDEs);
+	    // let more = envelope['$'];
+	    // console.log(more);
 	});
-
-	res.send('200');
 
 });
