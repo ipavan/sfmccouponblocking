06b104830da3e301d41290bdd718a538ed98675a
Torben Sominka <tsominka@internal.salesforce.com>
2019-04-25 11:32:43 +0100

    make it better

--- public/js/customjs.js
+++ public/js/customjs.js
@@ -50,11 +50,10 @@ $(document).ready(function(){
 
     function setSDKData() {
     	var sel = $('#dename').val();
-    	console.log(sel);
-    	console.log($("#radio-11").is(":checked"))
-    	console.log($("#radio-12").is(":checked"))
 
     	let newCoupon = $("#radio-11").is(":checked");
+		let textColor = $('#text-color').val();
+		let buttonColor = $('#button-color').val();
 
     	if (!newCoupon) {
     		let output = `<style type="text/css">.amp {font-size:1px;color:#f2f2f2;}</style>
@@ -68,14 +67,14 @@ $(document).ready(function(){
 <div class="code_none" id="cb-block-code"><table align="center" border="0" cellpadding="0" cellspacing="0" class="tmp--container" style="background-color:#ffffff;" width="100%">
 						    <tbody><tr>
 						      <td align="left" style="color:#4f4f4f;font-family:Arial, sans-serif;font-size:16px;font-style:normal;font-weight:normal;line-height:20px;padding:20px;text-align:center;vertical-align:top;" width="100%">
-						        <span style="color:#DD0000;"><span style="display: inline-block; font-family: Arial, sans-serif; font-size: 18px; font-style: normal; font-weight: bold ; padding: 5px 0px 10px;">Your voucher code is:</span></span> 
+						        <span style="color:${textColor};"><span style="display: inline-block; font-family: Arial, sans-serif; font-size: 18px; font-style: normal; font-weight: bold ; padding: 5px 0px 10px;">Your voucher code is:</span></span> 
 						        <!-- ============================== -->
 						        <!-- CTA BTN -->
 						        <!-- ============================== -->
 						        <table align="center" class="tmp--full-width" width="100%">
 						          <tbody><tr>
 						            <td align="center" style="padding:5px 0 10px 0;">
-						              <span style="font-family:courier new,courier,monospace;"><a href="" style="border-radius:3px;background-color:#DD0000;border-top:12px solid #DD0000;border-bottom:12px solid #DD0000;border-right:18px solid #DD0000;border-left:18px solid #DD0000;color:#ffffff;display:inline-block;font-family:Arial, sans-serif;font-size:16px;font-style:normal;font-weight:normal;line-height:16px;text-align:center;text-decoration:none;" target="_blank"><b>%%[ SET @CustomerCoupon = LookupOrderedRows('${sel}', 1, 'CouponCode DESC', 'EmailAddress', emailaddr) VAR @Coupon VAR @IsUsed IF RowCount(@CustomerCoupon) == 0 THEN SET @CouponRow = ClaimRow('${sel}', 'IsClaimed', 'EmailAddress', emailaddr, 'Validity', DateAdd(NOW(), '31', 'D')) IF EMPTY(@CouponRow) THEN SET @Coupon = 'No coupons available' ELSE SET @Coupon = FIELD(@CouponRow, 'CouponCode') ENDIF ELSE SET @Coupon = Lookup('${sel}', 'CouponCode', 'EmailAddress', emailaddr) SET @IsUsed = Lookup('${sel}', 'UsedDate', 'CouponCode', @Coupon) IF EMPTY(@IsUsed) THEN SET @ValidDate = Lookup('${sel}', 'Validity', 'EmailAddress', emailaddr) IF DateDiff(NOW(), @ValidDate, 'MI') < 0 THEN SET @Coupon = 'Coupon is expired.' ENDIF ELSE SET @Coupon = 'Sie haben Ihren Coupon schon genutzt' ENDIF ENDIF ]%% %%=v(@Coupon)=%%</b></a></span>
+						              <span style="font-family:courier new,courier,monospace;"><a href="" style="border-radius:3px;background-color:${buttonColor};border-top:12px solid ${buttonColor};border-bottom:12px solid ${buttonColor};border-right:18px solid ${buttonColor};border-left:18px solid ${buttonColor};color:#ffffff;display:inline-block;font-family:Arial, sans-serif;font-size:16px;font-style:normal;font-weight:normal;line-height:16px;text-align:center;text-decoration:none;" target="_blank"><b>%%[ SET @CustomerCoupon = LookupOrderedRows('${sel}', 1, 'CouponCode DESC', 'EmailAddress', emailaddr) VAR @Coupon VAR @IsUsed IF RowCount(@CustomerCoupon) == 0 THEN SET @CouponRow = ClaimRow('${sel}', 'IsClaimed', 'EmailAddress', emailaddr, 'Validity', DateAdd(NOW(), '31', 'D')) IF EMPTY(@CouponRow) THEN SET @Coupon = 'No coupons available' ELSE SET @Coupon = FIELD(@CouponRow, 'CouponCode') ENDIF ELSE SET @Coupon = Lookup('${sel}', 'CouponCode', 'EmailAddress', emailaddr) SET @IsUsed = Lookup('${sel}', 'UsedDate', 'CouponCode', @Coupon) IF EMPTY(@IsUsed) THEN SET @ValidDate = Lookup('${sel}', 'Validity', 'EmailAddress', emailaddr) IF DateDiff(NOW(), @ValidDate, 'MI') < 0 THEN SET @Coupon = 'Coupon is expired.' ENDIF ELSE SET @Coupon = 'Sie haben Ihren Coupon schon genutzt' ENDIF ENDIF ]%% %%=v(@Coupon)=%%</b></a></span>
 						            </td>
 						          </tr>
 						        </tbody></table>
@@ -94,14 +93,14 @@ $(document).ready(function(){
 <div class="code_none" id="cb-block-code"><table align="center" border="0" cellpadding="0" cellspacing="0" class="tmp--container" style="background-color:#ffffff;" width="100%">
 						    <tbody><tr>
 						      <td align="left" style="color:#4f4f4f;font-family:Arial, sans-serif;font-size:16px;font-style:normal;font-weight:normal;line-height:20px;padding:20px;text-align:center;vertical-align:top;" width="100%">
-						        <span style="color:#DD0000;"><span style="display: inline-block; font-family: Arial, sans-serif; font-size: 18px; font-style: normal; font-weight: bold; padding: 5px 0px 10px;">Your voucher code is: </span></span> 
+						        <span style="color:${textColor};"><span style="display: inline-block; font-family: Arial, sans-serif; font-size: 18px; font-style: normal; font-weight: bold; padding: 5px 0px 10px;">Your voucher code is: </span></span> 
 						        <!-- ============================== -->
 						        <!-- CTA BTN -->
 						        <!-- ============================== -->
 						        <table align="center" class="tmp--full-width" width="100%">
 						          <tbody><tr>
 						            <td align="center" style="padding:5px 0 10px 0;">
-						              <span style="font-family:courier new,courier,monospace;"><a href="" style="border-radius:3px;background-color:#DD0000;border-top:12px solid #DD0000;border-bottom:12px solid #DD0000;border-right:18px solid #DD0000;border-left:18px solid #DD0000;color:#ffffff;display:inline-block;font-family:Arial, sans-serif;font-size:16px;font-style:normal;font-weight:normal;line-height:16px;text-align:center;text-decoration:none;" target="_blank"><b>%%[ SET @CouponRow = ClaimRow('${sel}', 'IsClaimed', 'EmailAddress', emailaddr, 'Validity', DateAdd(NOW(), '31', 'D')) IF EMPTY(@CouponRow) THEN SET @Coupon = 'No coupons available' ELSE SET @Coupon = FIELD(@CouponRow, 'CouponCode') ENDIF ]%% %%=v(@Coupon)=%%</b></a></span>
+						              <span style="font-family:courier new,courier,monospace;"><a href="" style="border-radius:3px;background-color:${buttonColor};border-top:12px solid ${buttonColor};border-bottom:12px solid ${buttonColor};border-right:18px solid ${buttonColor};border-left:18px solid ${buttonColor};color:#ffffff;display:inline-block;font-family:Arial, sans-serif;font-size:16px;font-style:normal;font-weight:normal;line-height:16px;text-align:center;text-decoration:none;" target="_blank"><b>%%[ SET @CouponRow = ClaimRow('${sel}', 'IsClaimed', 'EmailAddress', emailaddr, 'Validity', DateAdd(NOW(), '31', 'D')) IF EMPTY(@CouponRow) THEN SET @Coupon = 'No coupons available' ELSE SET @Coupon = FIELD(@CouponRow, 'CouponCode') ENDIF ]%% %%=v(@Coupon)=%%</b></a></span>
 						            </td>
 						          </tr>
 						        </tbody></table>
