9ff6e98aceccab5823602e509e4c15fade3396b2
Torben Sominka <tsominka@internal.salesforce.com>
2019-05-03 16:29:04 +0100

    make it better

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/getDataExtensions', (req, res) => {
             uri:  'https://mct181ddvnpf05z2r5mcclnpt34q.auth.marketingcloudapis.com/v2/token',
             body: {
               'grant_type': 'client_credentials',
-              'client_id': vclientid,
+              'client_id': 'nwbiy6rkgktykoquuixgjhdg',
               'client_secret': '1GFFAQ4jNLqo5WTJaiCkqlaJ'
             },
             json: true
