5e577dcf44548bc7f5a8fe88c22d5ee84ccd4809
Pavan Sadarangani <psadarangani@psadaranga-ltm4.internal.salesforce.com>
2019-07-17 14:49:37 +0100

    fixes

--- server.js
+++ server.js
@@ -33,58 +33,67 @@ global.marketingCloudAccessToken = {
 	isExpired: true
 }
 
-app.get('/getDataExtensions', (req, res) => {
-	var options = {
-            method: 'POST',
-            uri:  `https://${process.env.ENDPOINT}.auth.marketingcloudapis.com/v2/token`,
-            body: {
-              'grant_type': 'client_credentials',
-              'client_id': `${process.env.CLIENTID}`,
-              'client_secret': `${process.env.CLIENT_SECRET}`
-            },
-            json: true
-        }
+// app.get('/getDataExtensions', (req, res) => {
+// 	var options = {
+//             method: 'POST',
+//             uri:  `https://${process.env.ENDPOINT}.auth.marketingcloudapis.com/v2/token`,
+//             body: {
+//               'grant_type': 'client_credentials',
+//               'client_id': `${process.env.CLIENTID}`,
+//               'client_secret': `${process.env.CLIENT_SECRET}`
+//             },
+//             json: true
+//         }
 
-    console.log(options);
+//     console.log(options);
 
-    request(options)
-    	.then(function(resp) {
-    		const accessToken = resp['access_token'];
-            console.log(`Access Token is ${accessToken}`);
+//     request(options)
+//     	.then(function(resp) {
+//     		const accessToken = resp['access_token'];
+//             console.log(`Access Token is ${accessToken}`);
 
-            const xml = `<?xml version="1.0" encoding="UTF-8"?><s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><s:Header><a:Action s:mustUnderstand="1">Retrieve</a:Action><a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID><a:ReplyTo><a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address></a:ReplyTo><a:To s:mustUnderstand="1">https://${process.env.ENDPOINT}.soap.marketingcloudapis.com/Service.asmx</a:To><fueloauth xmlns="http://exacttarget.com">${accessToken}</fueloauth></s:Header><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI"><RetrieveRequest><ObjectType>DataExtension</ObjectType><Properties>Name</Properties><Filter xsi:type="SimpleFilterPart"><Property>CategoryID</Property><SimpleOperator>equals</SimpleOperator><Value>${process.env.DE_ID}</Value> </Filter></RetrieveRequest></RetrieveRequestMsg></s:Body></s:Envelope>`
+//             const xml = `<?xml version="1.0" encoding="UTF-8"?><s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><s:Header><a:Action s:mustUnderstand="1">Retrieve</a:Action><a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID><a:ReplyTo><a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address></a:ReplyTo><a:To s:mustUnderstand="1">https://${process.env.ENDPOINT}.soap.marketingcloudapis.com/Service.asmx</a:To><fueloauth xmlns="http://exacttarget.com">${accessToken}</fueloauth></s:Header><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI"><RetrieveRequest><ObjectType>DataExtension</ObjectType><Properties>Name</Properties><Filter xsi:type="SimpleFilterPart"><Property>CategoryID</Property><SimpleOperator>equals</SimpleOperator><Value>${process.env.DE_ID}</Value> </Filter></RetrieveRequest></RetrieveRequestMsg></s:Body></s:Envelope>`
 
-            var soapOptions = {
-				    method: 'POST',
-				    uri: `https://${process.env.ENDPOINT}.soap.marketingcloudapis.com/Service.asmx`,
-				    headers: {
-				        'Content-Type': 'text/xml'
-				    },
-				    body: xml,
-				    json: false 
-				};
+//             var soapOptions = {
+// 				    method: 'POST',
+// 				    uri: `https://${process.env.ENDPOINT}.soap.marketingcloudapis.com/Service.asmx`,
+// 				    headers: {
+// 				        'Content-Type': 'text/xml'
+// 				    },
+// 				    body: xml,
+// 				    json: false 
+// 				};
 
-			let voucherDEs = [];
-			request(soapOptions)
-				.then(function(response) {
-					parseString(response, function (err, result) {
-					    let envelope = result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0];
-					    let results = envelope['Results'];
-					    results.forEach(function (item) {
-					    	let name = item['Name'][0]
-						    //console.log(name);
-						    voucherDEs.push(name);
-						});
-						res.send(voucherDEs);
-					});
-				})
-				.catch(function(error) {
-					console.log(error);
-				})
-    	})
-    	.catch(function(err){
-    		console.log(err);
-    	});
+// 			let voucherDEs = [];
+// 			request(soapOptions)
+// 				.then(function(response) {
+// 					parseString(response, function (err, result) {
+// 					    let envelope = result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0];
+// 					    let results = envelope['Results'];
+// 					    results.forEach(function (item) {
+// 					    	let name = item['Name'][0]
+// 						    //console.log(name);
+// 						    voucherDEs.push(name);
+// 						});
+// 						res.send(voucherDEs);
+// 					});
+// 				})
+// 				.catch(function(error) {
+// 					console.log(error);
+// 				})
+//     	})
+//     	.catch(function(err){
+//     		console.log(err);
+//     	});
+// });
+
+app.get('/getLanguage', (req, res) => {
+	if (process.env.LANGUAGE) {
+		res.send(process.env.LANGUAGE);
+	} else {
+		res.send('English');
+	}
+
 });
 
 
@@ -111,9 +120,7 @@ app.get('/getDEs', (req, res) => {
 			console.log(global.marketingCloudAccessToken);
 			//get journeys
 			getListOfDataExtensions(global.marketingCloudAccessToken.access_token).then((response) => {
-				console.log(response);
 				parseString(response['body'], function (err, result) {
-					console.log(result);
 				    let envelope = result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0];
 				    let results = envelope['Results'];
 				    results.forEach(function (item) {
@@ -132,7 +139,6 @@ app.get('/getDEs', (req, res) => {
 		getListOfDataExtensions(global.marketingCloudAccessToken.access_token).then((response) => {
 
 			parseString(response['body'], function (err, result) {
-				console.log(result);
 			    let envelope = result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0];
 			    let results = envelope['Results'];
 			    results.forEach(function (item) {
